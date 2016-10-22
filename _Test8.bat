fda629ef3a0434bdb7b0e49d2918b9cf3b8f357e
Andrew Ferlitsch <Andrew Ferlitsch>
2016-10-22 10:06:38 -0700

    test for large input

---
+++ _Test8.bat
@@ -0,0 +1,31 @@
+echo off 
+echo "Testing Large Files"
+del \tmp\tmp.*
+
+echo "Test: Binary, city-zip 25K records"
+java epipog -itests\cityzip.csv -S "state:string32,city:string32,postal:integer,latitude:float,longitude:float,number of poi:short"
+if %ERRORLEVEL% NEQ 0 ( echo FAILED rc ) else echo PASSED
+java epipog -s "city,postal" >stdout
+if %ERRORLEVEL% NEQ 0 ( echo FAILED rc ) else echo PASSED
+fc stdout tests\8.cmp >tmp
+if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
+
+echo "Test: CSV, city-zip 25K records"
+del \tmp\tmp.*
+java epipog -itests\cityzip.csv -S "state:string32,city:string32,postal:integer,latitude:float,longitude:float,number of poi:short" -d csv
+if %ERRORLEVEL% NEQ 0 ( echo FAILED rc ) else echo PASSED
+java epipog -s "city,postal" -d csv >stdout
+if %ERRORLEVEL% NEQ 0 ( echo FAILED rc ) else echo PASSED
+fc stdout tests\8.cmp >tmp
+if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
+
+echo "Test: JSON, city-zip 25K records"
+del \tmp\tmp.*
+java epipog -itests\cityzip.csv -S "state:string32,city:string32,postal:integer,latitude:float,longitude:float,number of poi:short" -d json
+if %ERRORLEVEL% NEQ 0 ( echo FAILED rc ) else echo PASSED
+java epipog -s "city,postal" -d json >stdout
+if %ERRORLEVEL% NEQ 0 ( echo FAILED rc ) else echo PASSED
+fc stdout tests\8.cmp >tmp
+if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
+
+del tmp stdout stderr
