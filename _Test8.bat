5a33bbe70b5f46df06e8afcb13ee9aabcc3cf897
Andrew Ferlitsch <Andrew Ferlitsch>
2016-10-27 10:26:06 -0700

    added test and updated README

--- _Test8.bat
+++ _Test8.bat
@@ -9,6 +9,10 @@ java epipog -s "city,postal" >stdout
 if %ERRORLEVEL% NEQ 0 ( echo FAILED rc ) else echo PASSED
 fc stdout tests\8.cmp >tmp
 if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
+java epipog -s "city,postal" -f "state=OR">stdout
+if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
+fc stdout tests\8a.cmp >tmp
+if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
 
 echo "Test: CSV, city-zip 25K records"
 del \tmp\tmp.*
@@ -18,6 +22,10 @@ java epipog -s "city,postal" -d csv >stdout
 if %ERRORLEVEL% NEQ 0 ( echo FAILED rc ) else echo PASSED
 fc stdout tests\8.cmp >tmp
 if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
+java epipog -s "city,postal,state" -f "state=OR" -d csv>stdout
+if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
+fc stdout tests\8b.cmp >tmp
+if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
 
 echo "Test: JSON, city-zip 25K records"
 del \tmp\tmp.*
@@ -26,6 +34,10 @@ if %ERRORLEVEL% NEQ 0 ( echo FAILED rc ) else echo PASSED
 java epipog -s "city,postal" -d json >stdout
 if %ERRORLEVEL% NEQ 0 ( echo FAILED rc ) else echo PASSED
 fc stdout tests\8.cmp >tmp
+if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
+java epipog -s "city,postal,state" -f "state=OR" -d json>stdout
+if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
+fc stdout tests\8b.cmp >tmp
 if %ERRORLEVEL% NEQ 0 ( echo FAILED fc ) else echo PASSED
 
 del tmp stdout stderr
